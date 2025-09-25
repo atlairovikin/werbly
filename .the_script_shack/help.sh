5c3e583e0b6431452e0d9e55539a0faad64beeeb
atlairovikin <nicolas.j.st.pierre@gmail.com>
2025-09-25 00:44:12 -0500

    Update help.sh

--- .the_script_shack/help.sh
+++ .the_script_shack/help.sh
@@ -5,25 +5,27 @@ printf '\n%s\n' '---------------------------------------------------------------
 #outputs following contents formated for visibility
 fold -s -w '66' << 'EndOfHeredoc' | sed -e 's/^[^[:space:]]/      &/'
 
-  werb [( help )|( [hail] [<query>] )|( [un]define [brew|<formula>] )|( [de|un]init )|( [un]install )|( [un]apply <cask> )|( man [<num1>] [<num2>] )]:
+  werb [ help | hail [<query>] | [un]define [brew|<formula>] ... | [de|un]init | [un]install | [un]apply <cask> ... | man <num1> [<num2>] ]:
 
     :
-      If no subsequent command is given, or if the subsequent command is null, then the call is treated as a ping, and the response 'Here!' is given.
+      If no subsequent command is given, or if the subsequent command is null, then the call is treated as a ping and the response 'Here!' is given.
 
     help:
       Display this message.
 
     hail <query>:
       Requests verdict from user, using argument <query> as prompt. Made only for internal use.
+      -
       If no argument is provided, or if argument is null, then the prompt 'Are you sure?' is assumed by default.
 
-    define [brew|<formula>]:
+    define [brew|<formula>] ...:
       Defines argument as respective executable.
       For example, `werb define brew` would define 'brew' as an alias of executable './werbly/the_brewery/homebrew/bin/brew'.
-      If no argument is provided, or if argument is null, then 'brew' is assumed by default.
     undefine [brew]|<formula>:
       Treats argument as a defined alias and undefines it.
+      -
       If no argument is provided, or if argument is null, then 'brew' is assumed by default.
+      Accepts multiple arguments, will operate on each argument individually until there are none left.
 
     init:
       Initializes and then verifies brew's enviroment as desired for a sandbox experience.
@@ -40,14 +42,17 @@ fold -s -w '66' << 'EndOfHeredoc' | sed -e 's/^[^[:space:]]/      &/'
       Treats argument <cask> as a would-be application, creating a link in the User's local Applications folder which then points towards the sandboxed cask, making the cask visable as an app.
     unapply <cask>:
       Removes argument <cask> as an application. This does not remove the cask itself, but rahter removes only the symlink file in the User's local Applications folder, causing the cask to no longer be visible as an app.
+      -
+      Accepts multiple arguments, will operate on each argument individually until there are none left.
 
-    man <num1> <num2>:
+    man <num1> [<num2>]:
       Prints homebrew's manual file, lines <num1> through <num2>.
       For example, to review Homebrew terminology (as of writing this), run `werb man 1 70`.
       This command serves as an alternative to homebrew's native `man brew` command, which will not work due to homebrew's isolated state.
+      Note that the manual is formated in markdown, yet is printed as raw text, often resulting in jumbled text (obviously not ideal, but I've yet to realize a better alternative).
+      -
       If only one argument is provided, then it is treated as the second argument and the first argument is assumed as '1'.
       If no arguments are provided, or if the provided arguments are null, then it ouputs the pathname and line count of the manpage.
-      Note that the manual is formated in markdown, yet is printed as raw text, often resulting in jumbled text (obviously not ideal, but I've yet to realize a better alternative).
 
 EndOfHeredoc
 #buffer
