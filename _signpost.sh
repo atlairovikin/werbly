f4e93a176dab2a883511672ce41b17ee17f33991
atlairovikin <nicolas.j.st.pierre@gmail.com>
2025-09-29 11:06:50 -0500

    Update _signpost.sh

--- _signpost.sh
+++ _signpost.sh
@@ -4,7 +4,7 @@
 export 'a_playground_dir'="${HOME}/werbly"
 #variable that Werbly uses to find the install-to directory
 export 'a_sandbox_dir'="${a_playground_dir}/the_brewery"
-#variable that Werbly uses to find it's scripts
+#variable that Werbly uses to find its scripts
 export 'a_script_dir'="${a_playground_dir}/.the_script_shack"
-#variable that Werbly uses to determine the location of the "Applications" folder
+#variable that Werbly uses to determine the location of the "Applications" folder, for use only by 'werb [un]apply <cask>'
 export 'an_app_dir'="${HOME}/Applications"
