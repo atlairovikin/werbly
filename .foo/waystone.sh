ea96d6e7b327def92e46fceb3f623638c2309d3d
atlairovikin <nicolas.j.st.pierre@gmail.com>
2025-10-29 18:51:05 -0500

    cleanup
    removed `werb get`

--- .foo/waystone.sh
+++ .foo/waystone.sh
@@ -3,25 +3,25 @@
 werb () {
     if test "${1}" == 'abort'; then
         unset -f 'werb'
-	printf 'Aborted!\n'
-	return 0
+        printf 'Aborted!\n'
+        return 0
     fi
     _werb="${1}" && shift 1
     case "${_werb}" in
         '')
-	    printf 'Here!\n';;
+            printf 'Here!\n';;
         help)
-	    cat "${werblyPath}/.foo/help.txt";;
+            cat "${werblyPath}/.foo/help.txt";;
         install|uninstall|init|deinit|show|man|exit)
-	    . "${werblyPath}/.foo/${_werb}.sh";;
-        define|undefine|apply|unapply|get)
-	    . "${werblyPath}/.foo/${_werb}.sh" "${@}";;
-	# for old/renamed commands (since initial release)
-	uninit|list|fetch)
-	    . "${werblyPath}/.foo/_${_werb}.sh";;
+            . "${werblyPath}/.foo/${_werb}.sh";;
+        define|undefine|apply|unapply)
+            . "${werblyPath}/.foo/${_werb}.sh" "${@}";;
+        # for old/renamed commands (since initial release)
+        uninit|list|fetch)
+            . "${werblyPath}/.foo/_${_werb}.sh";;
         *)
-	    printf 'Error: Unknown argument; Halting.\n' 1>&2
-	    printf 'See `werb help` for known arguments.\n';;
+            printf 'Error: Unknown argument; Halting.\n' 1>&2
+            printf 'See `werb help` for known arguments.\n';;
     esac
     unset -v '_werb'
 }
