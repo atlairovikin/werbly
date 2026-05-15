1a75471cd819def2ade0d27346e4bf23e9534680
atlairovikin <nicolas.j.st.pierre@gmail.com>
2026-05-15 14:33:32 -0500

    Update show.sh

--- .foo/show.sh
+++ .foo/show.sh
@@ -1,17 +1,17 @@
 printf 'Working...\n' && {
 
-    set "${werblyPath}/bar" "${HOME}/Applications"
+    set "${HOME}/Applications" "${werblyPath}/bar"
     [ -d "$1" ] || {
-        printf '%s\n' "Error: Cannot find sandbox directory at [${1}]; Halting." >&2
+        printf '%s\n' "Error: Cannot find applications directory at [${1}]; Halting." >&2
         return 1
     }
     [ -d "$2" ] || {
-        printf '%s\n' "Error: Cannot find applications directory at [${2}]; Halting." >&2
+        printf '%s\n' "Error: Cannot find sandbox directory at [${2}]; Halting." >&2
         return 2
     }
-    printf '\n%s\n' "--- Werbly's Sandbox Folder ---"
+    printf '\n%s\n' "--- User's Applications Folder ---"
     ls -a "$1"
-    printf '%s\n' '---+---' "--- User's Applications Folder ---"
+    printf '%s\n' '---+---' "--- Werbly's Sandbox Folder ---"
     ls -a "$2"
     printf '%s\n\n' '---+---'
 
